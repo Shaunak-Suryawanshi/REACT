9d4295ea2decca0356d5c7925ec17953090709e4
shaunakk <callmeshaun@users.noreply.github.com>
2026-01-16 13:31:19 +0530

    CUSTOM

---
+++ customReact/customreact.js
@@ -0,0 +1,11 @@
+const reactElement = {
+  type :'a',
+  props:{
+    href:'https://www.example.com',
+    target:'_blank',
+  },
+  children:'Visit Example.com'
+}
+
+const mainContainer = document.querySelector('#root')
+
