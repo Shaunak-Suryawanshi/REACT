9bd9c5be554119e46737251ec0ac2c0163c54dba
shaunakk <callmeshaun@users.noreply.github.com>
2026-01-16 13:31:01 +0530

    01_BASICS

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Shaun from './Shaun'
+
+
+function App() {
+
+
+  return (
+    <>
+   <Shaun/>
+   <p>This is shaunak</p>
+   <h1>Hello Vite + React!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Shaun.jsx
@@ -0,0 +1,8 @@
+function Shaun(){
+
+  return(
+    <h3>Shaun's Component</h3>
+  )
+}
+
+export default Shaun;
